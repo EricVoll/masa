569e8867089ec98a5c9b3e56cc754df31adf1ba1
Eric Vollenweider <eric.vollenweider@swiss-mile.com>
2024-08-06 18:15:14 +0200

    ci

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,4 +1,4 @@
-pip install torch==2.1.2
-pip install mmcv==2.1.0 -f https://download.openmmlab.com/mmcv/dist/cu118/torch2.1/index.html
+python -m pip install torch==2.1.2
+python -m pip install mmcv==2.1.0 -f https://download.openmmlab.com/mmcv/dist/cu118/torch2.1/index.html
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-pip install -e $SCRIPT_DIR
+python -m pip install -e $SCRIPT_DIR
